3edae39cbf2009a5739583ad170596da104d996f
motivanmia <waniowo@gmail.com>
2025-07-30 20:44:24 +0800

    chore(file):部屬設定文件

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,45 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag === 'iconify-icon',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag === 'iconify-icon',
+          },
         },
-      },
-    }),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }),
+      vueDevTools(),
+    ],
+    base: env.VITE_BASE || '/',
+    build: {
+      outDir: env.VITE_OUT_DIR || 'dist',
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
           @use "@/assets/scss/base/variable" as *;
           @use "@/assets/scss/mixin/function" as *;
           @use "@/assets/scss/mixin/mixin" as *;
         `,
+        },
       },
     },
-  },
+  };
 });
+
+// export default defineConfig({
+
+// });
