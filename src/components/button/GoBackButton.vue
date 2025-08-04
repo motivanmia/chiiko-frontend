32c86b487a42f34c1cac673ce55f314571fa3c4f
hung <ff789456741@gmail.com>
2025-08-05 01:13:10 +0800

    feat(go-back-button): 新增回前頁按鈕

---
+++ src/components/button/GoBackButton.vue
@@ -0,0 +1,44 @@
+<script setup>
+  import { useRouter } from 'vue-router';
+  import Icon from '../common/Icon.vue';
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
+</script>
+
+<template>
+  <button
+    class="go-back"
+    @click="handleClick"
+  >
+    <Icon
+      icon-name="leftA"
+      class="go-back__icon"
+    />
+    <div class="go-back__title">回前頁</div>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  .go-back {
+    @include flex();
+    color: color(text, light);
+    background-color: color(backgroundColor, recipe);
+    padding: px(10) px(20);
+    border: unset;
+    border-radius: px(20);
+    cursor: pointer;
+
+    &__icon {
+      @include font-size(30);
+      margin-right: px(10);
+    }
+
+    &__title {
+      @include fontSet($size: px(20), $lh: 1.2, $ls: 0.1em);
+    }
+  }
+</style>
