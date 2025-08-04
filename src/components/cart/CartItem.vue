d09edc055a2a5f0ce6bc0e2dbdc91fc3a2386e54
hung <ff789456741@gmail.com>
2025-08-05 01:25:32 +0800

    feat(cart): 新增購物車商品

---
+++ src/components/cart/CartItem.vue
@@ -0,0 +1,151 @@
+<script setup>
+  import Icon from '../common/Icon.vue';
+  import QuantityControl from './QuantityControl.vue';
+
+  defineProps({
+    product: {
+      type: Object,
+      required: true,
+    },
+  });
+
+  defineEmits(['increase-quantity', 'decrease-quantity', 'remove-product']);
+</script>
+
+<template>
+  <div class="cart-item">
+    <div class="cart-item__pic">
+      <img
+        class="cart-item__image"
+        :src="product.image"
+        :alt="product.name"
+      />
+    </div>
+    <div class="cart-item__info">
+      <h3 class="cart-item__name">{{ product.name }}</h3>
+      <span class="cart-item__unit-price">
+        NT$
+        <span class="cart-item__unit-price-number">{{ product.price }}</span>
+      </span>
+    </div>
+    <QuantityControl
+      :quantity="product.quantity"
+      @increase="$emit('increase-quantity')"
+      @decrease="$emit('decrease-quantity')"
+      class="cart-item__quantity"
+    />
+    <div class="cart-item__subtotal">
+      NT$
+      <span class="cart-item__subtotal-number">{{ product.price * product.quantity }}</span>
+    </div>
+    <button
+      class="cart-item__remove"
+      @click="$emit('remove-product')"
+    >
+      <Icon icon-name="remove" />
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .cart-item {
+    display: grid;
+    grid-template-columns: px(110) 3fr 1fr 1fr px(30);
+    padding: px(20) 0;
+
+    & + & {
+      border-top: px(1) solid color(text, dark);
+    }
+
+    &__pic {
+      width: px(80);
+      height: px(80);
+      border-radius: px(20);
+      overflow: hidden;
+    }
+
+    &__image {
+      @include fit();
+      aspect-ratio: 1 / 1;
+    }
+
+    &__info {
+      @include flex();
+    }
+
+    &__name {
+      flex: 2;
+      @include fontSet($size: px(20), $lh: 1.2, $color: color(text, dark), $ls: 0.1em);
+    }
+
+    &__unit-price {
+      flex: 1;
+      text-align: center;
+      @include fontSet($size: px(16), $lh: 1.2, $color: color(text, dark), $ls: 0.1em);
+
+      &-number {
+        @include font-size(20);
+      }
+    }
+
+    &__subtotal {
+      @include flex();
+      @include fontSet($size: px(20), $lh: 1.2, $color: color(text, dark), $ls: 0.1em);
+      text-align: center;
+
+      &-number {
+        @include font-size(24);
+      }
+    }
+
+    &__remove {
+      width: px(30);
+      background: none;
+      border: none;
+      @include fontSet($size: px(20), $color: color(text, dark));
+      cursor: pointer;
+
+      &:hover {
+        color: color(text, error);
+      }
+    }
+  }
+
+  @include rwdmax(768) {
+    .cart-item {
+      position: relative;
+      grid-template-columns: px(95) 1fr;
+
+      border-bottom: 1px solid #f0f0f0;
+
+      &__info {
+        @include flex($d: column, $jc: space-evenly, $ai: flex-start);
+      }
+
+      &__name {
+        flex: unset;
+      }
+
+      &__unit-price {
+        flex: unset;
+      }
+
+      &__quantity {
+        margin-right: auto;
+        margin-top: px(20);
+      }
+
+      &__subtotal {
+        margin-left: auto;
+        margin-top: px(20);
+      }
+
+      &__remove {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: fit-content;
+      }
+    }
+  }
+</style>
---
+++ src/components/cart/QuantityControl.vue
@@ -0,0 +1,84 @@
+<script setup>
+  import Icon from '../common/Icon.vue';
+
+  defineProps({
+    quantity: {
+      type: Number,
+      required: true,
+    },
+  });
+
+  defineEmits(['decrease', 'increase']);
+</script>
+
+<template>
+  <div class="quantity-control">
+    <button
+      class="quantity-control__button quantity-control__button--decrease"
+      @click="$emit('decrease')"
+    >
+      <Icon
+        icon-name="minus"
+        class="quantity-control__icon"
+      />
+    </button>
+    <input
+      type="number"
+      class="quantity-control__input"
+      :value="quantity"
+      min="1"
+      readonly
+    />
+    <button
+      class="quantity-control__button quantity-control__button--increase"
+      @click="$emit('increase')"
+    >
+      <Icon
+        icon-name="plus"
+        class="quantity-control__icon"
+      />
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+
+  .quantity-control {
+    @include flex();
+
+    &__button {
+      background-color: white;
+      border: unset;
+      @include fontSet($size: px(20));
+      cursor: pointer;
+
+      &--decrease {
+        border-radius: px(20) 0 0 px(20);
+      }
+
+      &--increase {
+        border-radius: 0 px(20) px(20) 0;
+      }
+    }
+
+    &__input {
+      width: px(20);
+      border: unset;
+      text-align: center;
+      @include fontSet($size: px(20));
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+</style>
