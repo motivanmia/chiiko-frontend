c1c1a78afe30b428efda90a0b7465a0392e2e313
Grey-Liu <sugarfree5807@gmail.com>
2025-08-04 21:00:49 +0800

    feat(home):新增食材區塊

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,20 @@
 <script setup>
   import { ref } from 'vue';
   import SearchBar from '@/components/common/SearchBar.vue';
+
+  import ShareRecipeButton from '../components/button/ShareRecipeButton.vue';
+
+  import delicious from '../components/Delicious.vue';
+
+  import section_title from '../components/SectionTitle.vue';
+
+  import login from '../components/user/Login.vue';
+
   import hot from '@/components/home/HomeHot.vue';
   import IndexRecipeOverview from '@/components/home/IndexRecipeOverview.vue';
-  import ShareRecipeButton from '@/components/button/ShareRecipeButton.vue';
-  import delicious from '@/components/Delicious.vue';
-  import section_title from '@/components/SectionTitle.vue';
-  import login from '@/components/user/Login.vue';
-  import HotSearch from '@/components/common/HotSearch.vue';
 
   import { useRouter } from 'vue-router';
+  import HomeSchool from '@/components/home/HomeSchool.vue';
   const router = useRouter();
   function handleShareRecipe() {
     router.push('/RecipeEdit');
@@ -32,8 +37,17 @@
 
     <div class="search__section">
       <SearchBar />
-      <div class="hot-search-wrapper">
-        <HotSearch />
+      <div class="search__hot">
+        熱搜：
+        <span>
+          <a
+            v-for="item in hotSearch"
+            :key="hotSearch.keyitem"
+            :href="item.path"
+          >
+            {{ item.keyitem }}
+          </a>
+        </span>
       </div>
     </div>
 
@@ -53,6 +67,8 @@
       <template #click-me>點我</template>
       <template #share>分享食譜</template>
     </ShareRecipeButton>
+
+    <HomeSchool />
   </div>
 </template>
 
@@ -69,15 +85,6 @@
       height: 100%;
       object-fit: cover;
       z-index: -2;
-    }
-    @include rwdmax(1400) {
-      height: 850px;
-    }
-    @include rwdmax(1200) {
-      height: 750px;
-    }
-    @include rwdmax(768) {
-      height: 650px;
     }
   }
   .scoll-down {
@@ -100,23 +107,6 @@
       $color: color(text, base),
       $ls: 1.5px
     );
-    @include rwdmax(1400) {
-      width: 350px;
-      height: 160px;
-      padding-top: 12px;
-    }
-    @include rwdmax(1200) {
-      width: 300px;
-      height: 150px;
-      padding-top: 10px;
-      @include font-size(18);
-    }
-    @include rwdmax(768) {
-      width: 190px;
-      height: 140px;
-      padding-top: 10px;
-      @include font-size(14);
-    }
   }
   .search__section {
     position: absolute;
@@ -124,14 +114,27 @@
     top: 50%;
     right: 15%;
   }
-  .hot-search-wrapper {
-    --hot-search-text-shadow: 0 4px 4px rgba(0, 0, 0, 0.59);
-    --hot-search-color: #fff;
+  .search__hot {
+    margin-top: 25px;
+    text-shadow: 0 4px 4px rgba(0, 0, 0, 0.59);
+    display: flex;
+    @include fontSet($font: $basic-font, $fw: normal, $size: px(24), $color: #fff, $ls: 2px);
+    span {
+      display: flex;
+      gap: 15px;
+    }
+    a {
+      color: #fff;
+    }
   }
   #ShareRecipeButton {
-    position: fixed;
+    position: sticky;
     right: 20px;
-    bottom: 20px;
-    // z-index: 1;
+    bottom: 30px;
+    z-index: 1;
+    @include rwdmax(768) {
+      right: 0;
+      bottom: 10px;
+    }
   }
 </style>
