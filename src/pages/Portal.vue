72e4ad101b11ffe472afd01f3e7d0d8f3396785f
Lau-ra-177 <kwbby8@gmail.com>
2025-08-06 01:05:17 +0800

    feat(Portal.vue) 入口新增copyright

--- src/pages/Portal.vue
+++ src/pages/Portal.vue
@@ -1,40 +1,54 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <div id="background">
-    <a href="/home">
-      <img
-        src="../assets/image/footer/01.png"
-        alt=""
-      />
-      <span id="user">前台</span>
-    </a>
-    <a href="#">
-      <img
-        src="../assets/image/footer/03.png"
-        alt=""
-        id="tomato"
-      />
-      <span id="admin">後台</span>
-    </a>
+    <div id="main">
+      <a href="/home">
+        <img
+          src="../assets/image/footer/01.png"
+          alt=""
+        />
+        <span id="user">前台</span>
+      </a>
+      <a href="#">
+        <img
+          src="../assets/image/footer/03.png"
+          alt=""
+          id="tomato"
+        />
+        <span id="admin">後台</span>
+      </a>
+    </div>
+    <div class="main-footer__copyright">
+      本網站為緯育TibaMe前端設計工程師班第 92 期學員專題成果作品，本平台僅供學習、展示之用。
+      <br />
+      若有侵權疑慮，您可以私訊［緯育TibaMe］，後續會由專人協助處理。
+      <br />
+      © 2025 Chiiko. All rights reserved.
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
   #background {
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    min-height: 100vh;
     background-color: #d97c48;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 30vw;
-    padding: auto;
     z-index: 1;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    #main {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 15vw;
+      margin: auto;
+    }
     a {
       text-decoration: none;
       position: relative;
+      display: flex;
       #user,
       #admin {
         width: 100px;
@@ -50,11 +64,12 @@
         text-decoration: none;
         color: color(text, dark);
         position: relative;
-        bottom: 100px;
-        right: -200px;
+        bottom: -150px;
+        left: -60px;
       }
       #admin {
-        bottom: 150px;
+        bottom: -200px;
+        left: -60px;
       }
     }
     @keyframes Up {
@@ -89,5 +104,8 @@
         color: color(text, light);
       }
     }
+  }
+  .main-footer__copyright {
+    margin-bottom: 20px;
   }
 </style>
